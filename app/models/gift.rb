f8fcf45f1f2c45357da1bafb6461520ef0762f53
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-10-22 15:50:49 -0400

    added some styling

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,4 +1,6 @@
 class Gift < ApplicationRecord
   belongs_to :user
+  validates :name, :price, presence: true
+  validates :website, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "need http// or https// before " }
   # belongs_to :giver, class_name: "User", foreign_key: "giver_id", optional: true
 end
