f59472937993446e2c7a552d252314d50c99d150
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-10-25 13:42:05 -0400

    removed gifts route and updated existing routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,5 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show, :create, :new, :delete] do 
     resources :gifts, only: [:show, :index, :create, :new]
-  end
-  resources :gifts, only: [:show, :index, :create, :new]
-  
+  end  
 end
